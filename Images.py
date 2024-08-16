c40363660ecb7d51daea1201ad3b31eafd0cc91a
LonnieHarp <lonnharp@icloud.com>
2024-08-15 21:37:12 -0400

    updating images so that they can be called from all files

---
+++ Images.py
@@ -0,0 +1,10 @@
+from IPython.display import Image
+
+def ML_process():
+    return Image('Images_path/ML_process.png', width=750, height=750)
+def classification():
+    return Image('Images_path/Classification.png', width=750, height=750)
+def loss_functions():
+    return Image('Images_path/common_loss_functions.png', width=750, height=750)
+def nn_modules():
+    return Image('Images_path/nn_modules.png', width=750, height=750)
