5b4d0e92ac9420b63db8e12d1c0f30ba71aed555
LonnieHarp <lonnharp@icloud.com>
2024-08-28 03:26:46 -0400

    images now resizable on the fly

--- Images.py
+++ Images.py
@@ -1,12 +1,16 @@
 from IPython.display import Image
 
-def ML_process():
-    return Image('Images_path/ML_process.png', width=750, height=750)
-def classification():
-    return Image('Images_path/Classification.png', width=750, height=750)
-def loss_functions():
-    return Image('Images_path/common_loss_functions.png', width=750, height=750)
-def nn_modules():
-    return Image('Images_path/nn_modules.png', width=750, height=750)
-def training_loop():
-    return Image('Images_path/training_loop.png', width=750,height=750)
+def ML_process(w=750, h=750):
+    return Image('Images_path/ML_process.png', width=w, height=h)
+def classification(w=750, h=750):
+    return Image('Images_path/Classification.png', width=w, height=h)
+def loss_functions(w=750, h=750):
+    return Image('Images_path/common_loss_functions.png', width=w, height=h)
+def nn_modules(w=750, h=750):
+    return Image('Images_path/nn_modules.png', width=w, height=h)
+def training_loop(w=750, h=750):
+    return Image('Images_path/training_loop.png', width=w,height=h)
+def multiclass(w=750, h=750):
+    return Image('Images_path/multiclass.png', width=w, height=h)
+def class_eval(w=750, h=750):
+    return Image('Images_path/class_eval.png', width=w, height=h)
